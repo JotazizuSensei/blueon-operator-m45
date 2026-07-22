9f94dfe7d311d467c4aaf876c7b73b58c0cfcd33
JotazizuSensei <jota.zizu@gmail.com>
2026-07-22 15:31:20 +0100

    Refresh offline cache for Operator M45 v5

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='blueon-m45-v3';
-const ASSETS=['./','./index.html','./styles.css','./app.js','./manifest.webmanifest','./icons/icon-192.svg','./icons/icon-512.svg'];
+const CACHE='blueon-operator-v5';
+const ASSETS=['./','./index.html','./styles.css','./app.js','./manifest.webmanifest','./assets/blue-on-m45-logo.svg','./icons/icon-192.svg','./icons/icon-512.svg'];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting()))});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim()))});
-self.addEventListener('fetch',event=>{if(event.request.method!=='GET')return;event.respondWith(fetch(event.request).then(response=>{const clone=response.clone();caches.open(CACHE).then(cache=>cache.put(event.request,clone));return response}).catch(()=>caches.match(event.request).then(hit=>hit||caches.match('./index.html'))))});
+self.addEventListener('fetch',event=>{if(event.request.method!=='GET')return;event.respondWith(caches.match(event.request).then(hit=>hit||fetch(event.request).then(response=>{const clone=response.clone();caches.open(CACHE).then(cache=>cache.put(event.request,clone));return response}).catch(()=>caches.match('./index.html'))))});
